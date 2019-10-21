76edc3b7dc76d9bc49d5cd593ae3d5fd4f589f22
nikakarelets <v.karelets@gmail.com>
2019-10-21 20:04:17 +0300

    Fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,23 +3,17 @@ const carousel = document.querySelector('.carousel-container');
 let currentDot = document.querySelector(' .carousel__dot');
 const prevButton = document.querySelector('.carousel__btn_prev');
 const nextButton = document.querySelector('.carousel__btn_next');
-
-let i = 1;
-for(let li of carousel.querySelectorAll('li')) {
-  li.style.position = 'relative';
-  i++;
-}
+const listItems = carousel.querySelectorAll('li');
 
 let width = 330;
-let count = 1;
 
-let list = carousel.querySelector('ul');
-let listElements = carousel.querySelectorAll('li');
+const list = carousel.querySelector('ul');
+const listElements = listItems;
 
 let position = 0;
 
-prevButton.onclick = function() {
-  position += width * count;
+prevButton.addEventListener('click', function () {
+  position += width;
   position = Math.min(position, 0);
   list.style.marginLeft = position + 'px';
   if (!currentDot.previousElementSibling) {
@@ -29,18 +23,17 @@ prevButton.onclick = function() {
     currentDot = currentDot.previousElementSibling;
   }
   currentDot.classList.add('carousel__dot_active');
-};
+});
 
-nextButton.onclick = function() {
-  position -= width * count;
-  position = Math.max(position, -width * (listElements.length - count));
+nextButton.addEventListener('click', function() {
+  position -= width;
+  position = Math.max(position, -width * (listElements.length - 1));
   list.style.marginLeft = position + 'px';
   list.style.transition = 'margin-left 0.5s';
   if (!currentDot.nextElementSibling) {
     return
-  } else  {
-    currentDot.className = 'carousel__dot';
-    currentDot = currentDot.nextElementSibling;
   }
+  currentDot.className = 'carousel__dot';
+  currentDot = currentDot.nextElementSibling;
   currentDot.classList.add('carousel__dot_active');
-};
+});
