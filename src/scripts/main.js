27c0bd75b1a747128a09118fc69ae6a50407f1a0
Anna Kazantseva <annetastara@gmail.com>
2019-10-21 18:12:00 +0300

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,29 +10,27 @@ const width = 350;
 const count = 1;
 let position = 0;
 
-prevButton.onclick = function() {
+prevButton.addEventListener('click', function() {
   position += width * count;
   position = Math.min(position, 0);
   list.style.marginLeft = position + 'px';
   if (!dot.previousElementSibling) {
     return;
-  } else {
-    dot.className = 'carousel__dot';
-    dot = dot.previousElementSibling;
   }
+  dot.className = 'carousel__dot';
+  dot = dot.previousElementSibling;
   dot.classList.add('carousel__dot_active');
-};
+});
 
-nextButton.onclick = function() {
+nextButton.addEventListener('click', function() {
   position -= width * count;
   position = Math.max(position, -width * (listElement.length - count));
   list.style.marginLeft = position + 'px';
   list.style.transition = 'margin-left 0.5s';
   if (!dot.nextElementSibling) {
     return;
-  } else {
-    dot.className = 'carousel__dot';
-    dot = dot.nextElementSibling;
   }
+  dot.className = 'carousel__dot';
+  dot = dot.nextElementSibling;
   dot.classList.add('carousel__dot_active');
-};
+});
