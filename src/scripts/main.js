bef7e8c71dfdafdfd2576bbcb6869da1d05232ca
Viktoriia <jhunt@ukr.net>
2019-10-23 11:06:11 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,46 +6,43 @@ const carousel = document.querySelector('.carousel');
 const cards = document.querySelectorAll('.carousel__item');
 let dot = document.querySelector(' .carousel__dot');
 
-let i = 0;
+let position = 0;
+
+prevBtn.addEventListener('click', showPrevSlide);
+nextBtn.addEventListener('click', showNextSlide);
 
 function showNextSlide() {
-  if (i <= -((cards.length - 1) * 350)) {
+  if (position <= -((cards.length - 1) * 350)) {
     return;
   }
 
-  i -= 350;
+  position -= 350;
 
-  carousel.style.transform = `translateX(${i}px)`;
+  carousel.style.transform = `translateX(${position}px)`;
 
   if (!dot.nextElementSibling) {
     return;
-  } else {
-    dot.className = 'carousel__dot';
-    dot = dot.nextElementSibling;
   }
 
+  dot.className = 'carousel__dot';
+  dot = dot.nextElementSibling;
   dot.classList.add('carousel__dot_active');
 }
 
-nextBtn.addEventListener('click', showNextSlide);
-
 function showPrevSlide() {
-  if (i >= 0) {
+  if (position >= 0) {
     return;
   }
 
-  i += 350;
+  position += 350;
 
-  carousel.style.transform = `translateX(${i}px)`;
+  carousel.style.transform = `translateX(${position}px)`;
 
   if (!dot.previousElementSibling) {
     dot = document.querySelector('.carousel__dots-wrap .carousel__dot');
-  } else {
-    dot.className = 'carousel__dot';
-    dot = dot.previousElementSibling;
   }
 
+  dot.className = 'carousel__dot';
+  dot = dot.previousElementSibling;
   dot.classList.add('carousel__dot_active');
 }
-
-prevBtn.addEventListener('click', showPrevSlide);
