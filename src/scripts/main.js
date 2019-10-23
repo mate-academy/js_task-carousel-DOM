f90b2bb0b6902759025d1556f8c3c63eb77f4546
Simon <semen.zelenskiy@gmail.com>
2019-10-23 09:43:57 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,36 @@
 'use strict';
+
+const container = document.querySelector('.container');
+const prevButton = document.querySelector('.carousel__btn_prev');
+const nextButton = document.querySelector('.carousel__btn_next');
+const list = container.querySelector('ul');
+const listElement = container.querySelectorAll('li');
+let dot = document.querySelector('.carousel__dot');
+const width = 350;
+const count = 1;
+let position = 0;
+
+prevButton.addEventListener('click', function() {
+  position += width * count;
+  position = Math.min(position, 0);
+  list.style.marginLeft = position + 'px';
+  if (!dot.previousElementSibling) {
+    return;
+  }
+  dot.className = 'carousel__dot';
+  dot = dot.previousElementSibling;
+  dot.classList.add('carousel__dot_active');
+});
+
+nextButton.addEventListener('click', function() {
+  position -= width * count;
+  position = Math.max(position, -width * (listElement.length - count));
+  list.style.marginLeft = position + 'px';
+  list.style.transition = 'margin-left 0.5s';
+  if (!dot.nextElementSibling) {
+    return;
+  }
+  dot.className = 'carousel__dot';
+  dot = dot.nextElementSibling;
+  dot.classList.add('carousel__dot_active');
+});
