92b0eeadf1ba70167753473f12dddea5933e1578
ITStan <s.stanislavskaya@gmail.com>
2019-10-25 00:53:33 +0300

    Correction

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,28 +2,29 @@
 
 const prevButton = document.querySelector('.carousel__btn_prev');
 const nextButton = document.querySelector('.carousel__btn_next');
-const items = document.querySelectorAll('.carousel__item');
+const firstItem = document.querySelector('.carousel__item_first');
 const dots = document.querySelectorAll('.carousel__dot');
 let position = -20;
 const width = 350;
 let counter = 0;
+const itemsLength = 5;
 
 prevButton.addEventListener('click', () => {
   if (counter < 1) {
     return;
   }
   position -= width;
-  items[0].style.marginLeft = -position + 'px';
+  firstItem.style.marginLeft = -position + 'px';
   counter--;
   dotClassName();
 });
 
 nextButton.addEventListener('click', () => {
-  if (counter > 2) {
+  if (counter > itemsLength - 2) {
     return;
   }
   position += width;
-  items[0].style.marginLeft = -position + 'px';
+  firstItem.style.marginLeft = -position + 'px';
   counter++;
   dotClassName();
 });
