be890bbc1af14be3ca3e8105f28e732960151800
Iuliia Tsentylo <labliuk.maksym@gmail.com>
2020-02-07 15:18:00 +0200

    soluton

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const arrPrev = document.querySelector('.carousel__btn_prev');
+const arrNext = document.querySelector('.carousel__btn_next');
+const cards = document.querySelectorAll('.carousel__item');
+
+const carouselCards = document.querySelector('.carousel');
+let dotActive = null;
+
+let position = 0;
+
+arrPrev.addEventListener('click', e => {
+  if (position < 0) {
+    position += 340;
+    carouselCards.style.transform = `translateX(${position}px)`;
+
+    dotActive = document.querySelector('.carousel__dot_active');
+    dotActive.previousElementSibling.classList.add('carousel__dot_active');
+    dotActive.classList.remove('carousel__dot_active');
+  }
+});
+
+arrNext.addEventListener('click', e => {
+  if (position > -((cards.length - 1) * 340)) {
+    position += -340;
+    carouselCards.style.transform = `translateX(${position}px)`;
+
+    dotActive = document.querySelector('.carousel__dot_active');
+    dotActive.nextElementSibling.classList.add('carousel__dot_active');
+    dotActive.classList.remove('carousel__dot_active');
+  }
+});
