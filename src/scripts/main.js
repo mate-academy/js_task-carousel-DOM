75bcee0787679b1bfa5893f1c21cfabf2519b750
Dan Denysovets <alen250299@gmail.com>
2020-02-10 00:15:52 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,42 @@
 'use strict';
+
+const buttonPrev = document.querySelector('.carousel__btn_prev');
+const buttonNext = document.querySelector('.carousel__btn_next');
+const carouselItems = document.querySelectorAll('.carousel__item');
+const carousel = document.querySelector('.carousel__wrapper');
+const dots = document.querySelectorAll('.carousel__dot');
+const carouselItemsWidth = carouselItems[0].clientWidth;
+const carouselLength = carousel.scrollWidth - carouselItemsWidth;
+let currentPosition = 0;
+let dotIndex = 0;
+
+function slideToNext() {
+  if (currentPosition > -carouselLength) {
+    currentPosition -= carouselItemsWidth;
+  }
+
+  carouselItems[0].style.marginLeft = `${currentPosition}px`;
+  dotsIndex();
+  dots[dotIndex].classList.add('carousel__dot_active');
+  dots[dotIndex - 1].classList.remove('carousel__dot_active');
+};
+
+function slideToPrev() {
+  if (currentPosition < 0) {
+    currentPosition += carouselItemsWidth;
+  }
+
+  carouselItems[0].style.marginLeft = `${currentPosition}px`;
+  dotsIndex();
+  dots[dotIndex].classList.add('carousel__dot_active');
+  dots[dotIndex + 1].classList.remove('carousel__dot_active');
+};
+
+function dotsIndex() {
+  dotIndex = (currentPosition / carouselItemsWidth) * -1;
+
+  return dotIndex;
+};
+
+buttonNext.addEventListener('click', slideToNext);
+buttonPrev.addEventListener('click', slideToPrev);
