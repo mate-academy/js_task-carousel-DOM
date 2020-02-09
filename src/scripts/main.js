228752ff9bb2dc09798d382fd988c403a8ac78db
Bohdan Salenik <bohdan.salenik@gmail.com>
2020-02-09 10:33:17 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const carousel = document.querySelector('.carousel');
+const carouselItem = document.querySelectorAll('.carousel__item');
+//  buttons
+const prevBtn = document.querySelector('.carousel__btn_prev');
+const nextBtn = document.querySelector('.carousel__btn_next');
+// counter
+let counter = 0;
+const size = carouselItem[0].clientWidth;
+// buttonsListener
+
+nextBtn.addEventListener('click', () => {
+  carousel.style.transition = 'transform 0.4s ease-in-out';
+  counter++;
+  carousel.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+prevBtn.addEventListener('click', () => {
+  carousel.style.transition = 'transform 0.4s ease-in-out';
+  counter--;
+  carousel.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+carousel.addEventListener('transitionend', () => {
+  if (counter === carouselItem.length) {
+    carousel.style.transition = 'none';
+    counter = 0;
+    carousel.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  }
+
+  if (counter < 0) {
+    carousel.style.transition = 'none';
+    counter = carouselItem.length - 1;
+    carousel.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  }
+});
