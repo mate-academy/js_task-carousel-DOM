3e785280d7d478cb75c889115becc14c89ddcff3
Dan Denysovets <alen250299@gmail.com>
2020-02-10 14:11:13 +0200

    refactored

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,39 +3,35 @@
 const buttonPrev = document.querySelector('.carousel__btn_prev');
 const buttonNext = document.querySelector('.carousel__btn_next');
 const carouselItems = document.querySelectorAll('.carousel__item');
-const carousel = document.querySelector('.carousel__wrapper');
 const dots = document.querySelectorAll('.carousel__dot');
-const carouselItemsWidth = carouselItems[0].clientWidth;
-const carouselLength = carousel.scrollWidth - carouselItemsWidth;
+const carouselItemsWidth = carouselItems[0].clientWidth + 1;
 let currentPosition = 0;
-let dotIndex = 0;
+let counter = 0;
 
 function slideToNext() {
-  if (currentPosition > -carouselLength) {
-    currentPosition -= carouselItemsWidth;
+  if (counter > carouselItems.length - 2) {
+    return;
   }
 
-  carouselItems[0].style.marginLeft = `${currentPosition}px`;
-  dotsIndex();
-  dots[dotIndex].classList.add('carousel__dot_active');
-  dots[dotIndex - 1].classList.remove('carousel__dot_active');
+  counter += 1;
+  carouselSwitcher(counter);
+  dots[counter - 1].classList.remove('carousel__dot_active');
 };
 
 function slideToPrev() {
-  if (currentPosition < 0) {
-    currentPosition += carouselItemsWidth;
+  if (counter <= 0) {
+    return;
   }
 
-  carouselItems[0].style.marginLeft = `${currentPosition}px`;
-  dotsIndex();
-  dots[dotIndex].classList.add('carousel__dot_active');
-  dots[dotIndex + 1].classList.remove('carousel__dot_active');
+  counter -= 1;
+  carouselSwitcher(counter);
+  dots[counter + 1].classList.remove('carousel__dot_active');
 };
 
-function dotsIndex() {
-  dotIndex = (currentPosition / carouselItemsWidth) * -1;
-
-  return dotIndex;
+function carouselSwitcher(itemIndex) {
+  currentPosition = (carouselItemsWidth - 1) * -itemIndex;
+  carouselItems[0].style.marginLeft = `${currentPosition}px`;
+  dots[itemIndex].classList.add('carousel__dot_active');
 };
 
 buttonNext.addEventListener('click', slideToNext);
