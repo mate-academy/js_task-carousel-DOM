9e17945567edafd50fb27207c5b1ffa97b6ce35d
WincherZ <wincher2016@gmail.com>
2020-04-15 21:48:26 +0300

    some changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,46 +1,50 @@
 'use strict';
 
-const wrapper = document.querySelector('.wrapper');
-const itemGallery = document.querySelector('.carousel');
-const allItems = document.querySelectorAll('.carousel__item');
-let counter = 0;
-let counterArray = 1;
-const step = 330;
-const dots = document.querySelectorAll('.carousel__dot');
-const activeClass = 'carousel__dot_active';
+const createCarousel = () => {
+  const wrapper = document.querySelector('.wrapper');
+  const itemGallery = document.querySelector('.carousel');
+  const allItems = document.querySelectorAll('.carousel__item');
+  let counter = 0;
+  let counterArray = 1;
+  const step = 330;
+  const dots = document.querySelectorAll('.carousel__dot');
+  const activeClass = 'carousel__dot_active';
 
-wrapper.addEventListener('click', e => {
-  if (e.target.classList.contains('carousel__btn_prev')) {
-    counterArray--;
+  wrapper.addEventListener('click', e => {
+    if (e.target.classList.contains('carousel__btn_prev')) {
+      counterArray--;
 
-    if (counterArray <= 0) {
-      counterArray = allItems.length;
-      counter = -(step * allItems.length);
+      if (counterArray <= 0) {
+        counterArray = allItems.length;
+        counter = -(step * allItems.length);
+      }
+
+      counter += step;
+
+      for (const item of dots) {
+        item.classList.remove(activeClass);
+      }
+
+      dots[counterArray - 1].classList.add(activeClass);
+      itemGallery.style = `transform: translateX(${counter}px);`;
+    } else if (e.target.classList.contains('carousel__btn_next')) {
+      counter -= step;
+
+      if (counterArray >= allItems.length) {
+        counter = 0;
+        counterArray = 0;
+      }
+
+      counterArray++;
+
+      for (const item of dots) {
+        item.classList.remove(activeClass);
+      }
+
+      dots[counterArray - 1].classList.add(activeClass);
+      itemGallery.style = `transform: translateX(${counter}px);`;
     }
+  });
+};
 
-    counter += step;
-
-    for (const item of dots) {
-      item.classList.remove(activeClass);
-    }
-
-    dots[counterArray - 1].classList.add(activeClass);
-    itemGallery.style = `transform: translateX(${counter}px);`;
-  } else if (e.target.classList.contains('carousel__btn_next')) {
-    counter -= step;
-
-    if (counterArray >= allItems.length) {
-      counter = 0;
-      counterArray = 0;
-    }
-
-    counterArray++;
-
-    for (const item of dots) {
-      item.classList.remove(activeClass);
-    }
-
-    dots[counterArray - 1].classList.add(activeClass);
-    itemGallery.style = `transform: translateX(${counter}px);`;
-  }
-});
+createCarousel();
