5a1ccd08726094be4a0195d268069ed0b02da81a
crash2bash <sachagurov@gmail.com>
2020-04-16 16:07:13 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,28 +8,45 @@ const carouselDots = document.querySelectorAll('.carousel__dot');
 
 let count = 0;
 
-carouselButtons[1].addEventListener('click', (evt) => {
-  evt.preventDefault();
-
-  if (count === carouselItems.length - 1) {
-    return;
-  }
-
-  const target = evt.target;
-
-  scrollSlider(target, carousel);
-});
-
 carouselButtons[0].addEventListener('click', (evt) => {
   evt.preventDefault();
 
+  const target = evt.target;
+
   if (count === 0) {
+    carouselDots[count].classList.remove('carousel__dot_active');
+    count = carouselItems.length;
+    scrollSlider(target, carousel);
+
+    carouselDots[count].classList.add('carousel__dot_active');
+
     return;
   }
 
+  scrollSlider(target, carousel);
+
+  carouselDots[count + 1].classList.remove('carousel__dot_active');
+  carouselDots[count].classList.add('carousel__dot_active');
+});
+
+carouselButtons[1].addEventListener('click', (evt) => {
+  evt.preventDefault();
+
   const target = evt.target;
 
+  if (count === carouselItems.length - 1) {
+    carouselDots[count].classList.remove('carousel__dot_active');
+    count = -1;
+    scrollSlider(target, carousel);
+    carouselDots[count].classList.add('carousel__dot_active');
+
+    return;
+  }
+
   scrollSlider(target, carousel);
+
+  carouselDots[count - 1].classList.remove('carousel__dot_active');
+  carouselDots[count].classList.add('carousel__dot_active');
 });
 
 function scrollSlider(eventTarget, slide) {
@@ -37,13 +54,9 @@ function scrollSlider(eventTarget, slide) {
     if (eventTarget === carouselButtons[1]) {
       count++;
       item.style.transform = `translateX(${-carouselItemWidth * count}px)`;
-      carouselDots[count].classList.add('carousel__dot_active');
-      carouselDots[count - 1].classList.remove('carousel__dot_active');
     } else {
       count--;
       item.style.transform = `translateX(${-carouselItemWidth * count}px)`;
-      carouselDots[count].classList.add('carousel__dot_active');
-      carouselDots[count + 1].classList.remove('carousel__dot_active');
     }
   });
 }
