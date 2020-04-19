9230ffc2f29104871207d73c9e09ddfef0db5de9
Kostya <oliinyk.kostya@gmail.com>
2020-04-19 19:27:41 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const carouselWrap = document.querySelector('.carousel__wrap');
+const carousel = document.querySelector('.carousel');
+let index = 0;
+
+carouselWrap.addEventListener('click', () => {
+  const target = event.target;
+  const item = event.target.closest('.carousel__btn');
+
+  if (!item) {
+    return;
+  }
+
+  if (target.classList[1] === 'carousel__btn_next' & index <= 620) {
+    pesach(310);
+  }
+
+  if (target.classList[1] === 'carousel__btn_prev' & index >= 310) {
+    pesach(-310);
+  }
+});
+
+function pesach(item) {
+  index = index + item;
+  carousel.style.marginLeft = `-${index}px`;
+}
