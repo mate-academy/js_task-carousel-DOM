3f77b5fd447175c3d6d654db74b2d5650cb1d892
Alex Machula <alexmachulamail@gmail.com>
2020-04-20 13:43:18 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,60 @@
 'use strict';
+
+const btnPrev = document.querySelector('.carousel__btn_prev');
+const btnNext = document.querySelector('.carousel__btn_next');
+const carousel = document.querySelector('.carousel');
+const carouselItem = document.querySelector('.carousel__item');
+const dot = document.querySelectorAll('.carousel__dot');
+
+const widthCarousel = carousel.scrollWidth;
+const widthCard = carouselItem.clientWidth;
+
+let count = 0;
+let prevCount = 0;
+let position = 0;
+
+btnPrev.addEventListener('click', (ev) => {
+  const elementActive = ev.target.closest('.carousel__btn_prev');
+
+  if (!elementActive) {
+    return;
+  }
+
+  if (position === 0) {
+    position = -widthCarousel;
+    count = dot.length - 1;
+  }
+
+  if (count < 0) {
+    count = dot.length - 1;
+  }
+
+  position += widthCard;
+
+  carousel.style.transform = `translate(${position}px)`;
+  dot[prevCount].classList.remove('carousel__dot_active');
+  dot[count].classList.add('carousel__dot_active');
+  prevCount = count;
+  count--;
+});
+
+btnNext.addEventListener('click', (ev) => {
+  const elementActive = ev.target.closest('.carousel__btn_next');
+
+  if (!elementActive) {
+    return;
+  }
+
+  position -= widthCard;
+
+  if (-position > widthCarousel - widthCard) {
+    position = 0;
+    count = 0;
+  }
+
+  carousel.style.transform = `translate(${position}px)`;
+  dot[prevCount].classList.remove('carousel__dot_active');
+  dot[count].classList.add('carousel__dot_active');
+  prevCount = count;
+  count++;
+});
