0f8ea3a678c40f302e78a0ad6d3750f8747714fd
MykolaMak <makolamak@gmail.com>
2020-04-21 11:59:49 +0300

    develop carousel

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,39 @@
 'use strict';
+
+let position = 0;
+const step = 320;
+
+document.addEventListener('click', arrow);
+
+function arrow(ev) {
+  const item = document.querySelectorAll('.carousel__item');
+  const dots = document.querySelectorAll('.carousel__dot');
+
+  dots[position / step].classList.toggle('carousel__dot_active');
+
+  if (ev.target.className.includes('carousel__btn_next')) {
+    position += step;
+
+    if (position > 960) {
+      position = 0;
+    }
+
+    for (const el of item) {
+      el.style.right = `${position}px`;
+    }
+  }
+
+  if (ev.target.className.includes('carousel__btn_prev')) {
+    position -= step;
+
+    if (position < 0) {
+      position = 960;
+    }
+
+    for (const el of item) {
+      el.style.right = `${position}px`;
+    }
+  }
+
+  dots[position / step].classList.toggle('carousel__dot_active');
+}
