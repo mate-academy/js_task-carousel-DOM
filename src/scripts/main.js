6f333ac83f17cf8d6bd8f9c94df01c5aa3eedff5
Illia Oliynik <illiaoliynik@gmail.com>
2020-04-22 13:33:05 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,24 +12,33 @@ function initialCarusel() {
 
   nextBtn.addEventListener('click', () => {
     if (startIndex === listItems.length - 1) {
-      return;
-    }
-    dots[startIndex].classList.remove('carousel__dot_active');
-    startIndex++;
-    dots[startIndex].classList.add('carousel__dot_active');
+      dots[startIndex].classList.remove('carousel__dot_active');
+      startIndex = 0;
+      dots[startIndex].classList.add('carousel__dot_active');
+      listItems.forEach(elem => (elem.style.transform = ''));
+    } else {
+      dots[startIndex].classList.remove('carousel__dot_active');
+      startIndex++;
+      dots[startIndex].classList.add('carousel__dot_active');
 
-    listItems[startIndex].style.transform
-      = `translateX(${-310 * startIndex}px)`;
+      listItems[startIndex].style.transform
+        = `translateX(${-310 * startIndex}px)`;
+    }
   });
 
   prevBtn.addEventListener('click', () => {
     if (!startIndex) {
-      return;
-    }
-    dots[startIndex].classList.remove('carousel__dot_active');
+      dots[startIndex].classList.remove('carousel__dot_active');
+      startIndex = listItems.length - 1;
+      dots[startIndex].classList.add('carousel__dot_active');
 
-    listItems[startIndex].style.transform = `translateX(0)`;
-    startIndex--;
-    dots[startIndex].classList.add('carousel__dot_active');
+      listItems.forEach((elem, index) =>
+        (elem.style.transform = `translateX(${-310 * index}px)`));
+    } else {
+      dots[startIndex].classList.remove('carousel__dot_active');
+      listItems[startIndex].style.transform = `translateX(0)`;
+      startIndex--;
+      dots[startIndex].classList.add('carousel__dot_active');
+    }
   });
 }
