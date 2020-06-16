0be77f6667041d9760b95313867415e1f38a93bc
Liliia <chuiko1112@gmail.com>
2020-06-16 14:56:19 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const previous = document.querySelector('.carousel__btn_prev');
+const next = document.querySelector('.carousel__btn_next');
+const firstLi = document.querySelector('.first');
+let position = 0;
+
+previous.onclick = function() {
+  position += 310;
+
+  if (position > 0) {
+    position = -930;
+  }
+
+  firstLi.style.marginLeft = position + 'px';
+};
+
+next.onclick = function() {
+  position -= 310;
+
+  if (position < -1200) {
+    position = 0;
+  }
+  firstLi.style.marginLeft = position + 'px';
+};
