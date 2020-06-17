76f374ed40021a480755a0eb986ebd6bc05e6b78
denkondratiev <denys.kondratiev.job@gmail.com>
2020-06-17 13:19:46 +0300

    add moveDots

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,11 +4,16 @@ const carousel = document.querySelector('.carousel');
 const items = document.querySelectorAll('.carousel__item');
 const prev = document.querySelector('.carousel__btn_prev');
 const next = document.querySelector('.carousel__btn_next');
+const dots = document.querySelectorAll('.carousel__dot');
 const gap = 340;
 
 let position = 0;
+let i = 0;
 
-function slideLeft(e) {
+prev.addEventListener('click', slideLeft);
+next.addEventListener('click', slideRight);
+
+function slideRight(e) {
   position -= gap;
 
   if (Math.abs(position) >= items.length * gap) {
@@ -16,9 +21,22 @@ function slideLeft(e) {
   };
 
   carousel.style.transform = `translateX(${position}px)`;
+
+  function moveDotRight() {
+    dots[i].classList.toggle('carousel__dot_active');
+    i++;
+
+    if (i > items.length - 1) {
+      i = 0;
+    };
+
+    dots[i].classList.toggle('carousel__dot_active');
+  };
+
+  setTimeout(moveDotRight, 500);
 };
 
-function slideRight(e) {
+function slideLeft(e) {
   position += gap;
 
   if (position > 0) {
@@ -26,7 +44,17 @@ function slideRight(e) {
   };
 
   carousel.style.transform = `translateX(${position}px)`;
-};
 
-prev.addEventListener('click', slideLeft);
-next.addEventListener('click', slideRight);
+  function moveDotsLeft() {
+    dots[i].classList.toggle('carousel__dot_active');
+    i--;
+
+    if (i < 0) {
+      i = items.length - 1;
+    };
+
+    dots[i].classList.toggle('carousel__dot_active');
+  };
+
+  setTimeout(moveDotsLeft, 500);
+};
