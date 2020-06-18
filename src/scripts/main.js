5a2b238c9cf18739af0fe1306ed9b96af1890789
LimonovInsp <bis.limonov@gmail.com>
2020-06-18 11:58:18 +0300

    carousel

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const cardsNumber = document.querySelectorAll('.carousel__item').length;
+const cardsList = document.querySelector('.carousel__list');
+const cardsButtons = document.querySelectorAll('.carousel__btn');
+const cardsDots = document.querySelector('.carousel__dots-wrap').children;
+
+let numberIndex = 1;
+let translateX = 0;
+
+cardsButtons.forEach(button => {
+  button.addEventListener('click', e => {
+    if (e.target.id === 'previous') {
+      if (numberIndex !== 1) {
+        numberIndex--;
+        translateX += 310;
+        cardsDots[numberIndex - 1].style.opacity = '1';
+        cardsDots[numberIndex].style.opacity = '0.5';
+      }
+    } else {
+      if (numberIndex !== cardsNumber) {
+        numberIndex++;
+        translateX -= 310;
+        cardsDots[numberIndex - 1].style.opacity = '1';
+        cardsDots[numberIndex - 2].style.opacity = '0.5';
+      }
+    }
+
+    cardsList.style.transform = `translateX(${translateX}px)`;
+  });
+});
