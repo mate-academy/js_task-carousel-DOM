95ef316b07e984b2feb238d68fa73623e39f8992
kvalenty <krushunin89@gmail.com>
2020-06-19 16:05:58 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,21 +3,44 @@
 const btnNext = document.querySelector('.carousel__btn_next');
 const btnPrev = document.querySelector('.carousel__btn_prev');
 const list = document.querySelector('.carousel');
+const dots = document.querySelectorAll('.carousel__dot');
 const quantityItems = list.childElementCount - 1;
 
-const li = document.querySelector('li');
-const width = li.offsetWidth;
+const width = list.firstElementChild.offsetWidth;
 
 list.style.width = `${width}px`;
 
 let counter = 0;
+let previous = counter;
 
 btnNext.addEventListener('click', function(e) {
-  counter = (counter >= quantityItems) ? 0 : counter + 1;
-  list.style.transform = `translateX(-${counter * width}px)`;
+  countNext();
+
+  slideShow();
 });
 
 btnPrev.addEventListener('click', function(e) {
-  counter = (counter <= 0) ? (quantityItems) : counter - 1;
-  list.style.transform = `translateX(-${(counter) * width}px)`;
+  countPrev();
+
+  slideShow();
 });
+
+function countPrev() {
+  counter = (counter <= 0)
+    ? quantityItems
+    : (counter - 1);
+}
+
+function countNext() {
+  counter = (counter >= quantityItems)
+    ? 0
+    : (counter + 1);
+}
+
+function slideShow() {
+  dots.item(previous).classList.toggle('carousel__dot_active');
+  dots.item(counter).classList.toggle('carousel__dot_active');
+
+  list.style.transform = `translateX(-${(counter) * width}px)`;
+  previous = counter;
+}
